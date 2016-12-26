7eb5e12defee2736800b640c2253e9ca19103616
Patrice Boulanger <patrice.boulanger92@gmail.com>
2016-12-26 12:26:08 +0100

    change test for colinear points

--- optimizer.py
+++ optimizer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import math, sys
+import numpy as np
 
 class Optimizer:
     """ """
@@ -17,31 +18,33 @@ class Optimizer:
         ulen = math.sqrt(u[0] * u[0] + u[1] * u[1])
         vlen = math.sqrt(v[0] * v[0] + v[1] * v[1])
 
-        if ulen > 0.00001 and vlen > 0.00001:
-            u1, v1 = [ u[0] / ulen, u[1] / ulen ], [ v[0] / vlen, v[1] / vlen ]
-        else:
+        if np.isclose(ulen, 0) or np.isclose(vlen, 0):
             return True
         
-        return 1 - abs(u1[0] * v1[0] + u1[1] * v1[1]) < 0.00001
+        u1, v1 = [ u[0] / ulen, u[1] / ulen ], [ v[0] / vlen, v[1] / vlen ]
+        
+        return np.isclose(1, abs(u1[0] * v1[0] + u1[1] * v1[1]))
     
     def points_from_segments(self, segs):
-        """ Take a list of segments and organize them into one or several continuous list of points """
+        """ Take a list of segments and organize them into one or several continuous lists of points """
+
         paths = []
-        
+
         while len(segs) > 0:
             # List of tuples
             path = [] # [ (x,y), (x,y), ... (x,y) ]
             
-            # Initialize the path with the 2 first points 
+            # Initialize the path with the 2 first points
             path.append((segs[0][0], segs[0][1]))
             path.append((segs[0][2], segs[0][3]))
+                
             del segs[0]
 
             idx = 0
             while len(segs) > 0 and idx < len(segs):
                 s = segs[idx] # (xa, ya, xb, yb)
                 
-                if path[0] == (s[0], s[1]):
+                if np.isclose(path[0][0], s[0]) and np.isclose(path[0][1], s[1]):
                     if self.colinear(path[0], path[1], (s[2], s[3])):
                         path[0] = (s[2], s[3])
                     else:
@@ -49,7 +52,7 @@ class Optimizer:
                         
                     del segs[idx]
                     idx = -1
-                elif path[0] == (s[2], s[3]):
+                elif np.isclose(path[0][0], s[2]) and np.isclose(path[0][1], s[3]):
                     if self.colinear(path[0], path[1], (s[0], s[1])):
                         path[0] = (s[0], s[1])
                     else:
@@ -57,7 +60,7 @@ class Optimizer:
                         
                     del segs[idx]
                     idx = -1
-                elif path[-1] == (s[0], s[1]):
+                elif np.isclose(path[-1][0], s[0]) and np.isclose(path[-1][1], s[1]):
                     if self.colinear(path[-2], path[-1], (s[2], s[3])):
                         path[-1] = (s[2], s[3])
                     else:
@@ -65,7 +68,7 @@ class Optimizer:
                         
                     del segs[idx]
                     idx = -1
-                elif path[-1] == (s[2], s[3]):
+                elif np.isclose(path[-1][0], s[2]) and np.isclose(path[-1][1], s[3]):
                     if self.colinear(path[-2], path[-1], (s[0], s[1])):
                         path[-1] = (s[0], s[1])
                     else:
@@ -76,7 +79,10 @@ class Optimizer:
                     
                 idx = idx + 1
 
-            paths.append(path)
+            if len(path) > 1:
+                paths.append(path)
+            else:
+                print("path of length == 1")
 
         return paths
         
