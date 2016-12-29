d6e5f43734844b1e6d2212dce496c3e64d4af389
Patrice Boulanger <patrice.boulanger92@gmail.com>
2016-12-29 19:19:52 +0100

    move colinear to util.py

--- optimizer.py
+++ optimizer.py
@@ -4,7 +4,7 @@ import math, sys
 import numpy as np
 from timeit import default_timer as timer
 
-from util import fequals
+from util import fequals, colinear2d
 
 class Optimizer:
     """ """
@@ -12,22 +12,7 @@ class Optimizer:
     def __init__(self, config):
         """ Constructor """
         self.config = config
-
-    def colinear(self, p0, p1, p2):
-        """ Returns true if three 2d points are colinear """
         
-        u, v = [ p1[0] - p0[0], p1[1] - p0[1] ], [ p2[0] - p0[0], p2[1] - p0[1] ]
-
-        ulen = math.sqrt(u[0] * u[0] + u[1] * u[1])
-        vlen = math.sqrt(v[0] * v[0] + v[1] * v[1])
-
-        if fequals(ulen, 0) or fequals(vlen, 0):
-            return True
-        
-        u1, v1 = [ u[0] / ulen, u[1] / ulen ], [ v[0] / vlen, v[1] / vlen ]
-        
-        return fequals(1, abs(u1[0] * v1[0] + u1[1] * v1[1]))
-    
     def points_from_segments(self, segs):
         """ Take a list of segments and organize them into one or several continuous lists of points.
             If several successive points are aligned, reduced the number of points keeping only the endpoints. """
@@ -51,7 +36,7 @@ class Optimizer:
                 s = segs[idx] # (xa, ya, xb, yb)
                 
                 if fequals(path[0][0], s[0]) and fequals(path[0][1], s[1]):
-                    if self.colinear(path[0], path[1], (s[2], s[3])):
+                    if colinear2d(path[0][0], path[0][1], path[1][0], path[1][1], s[2], s[3]):
                         path[0] = (s[2], s[3])
                     else:
                         path.insert(0, (s[2], s[3]))
@@ -59,7 +44,7 @@ class Optimizer:
                     del segs[idx]
                     idx = -1
                 elif fequals(path[0][0], s[2]) and fequals(path[0][1], s[3]):
-                    if self.colinear(path[0], path[1], (s[0], s[1])):
+                    if colinear2d(path[0][0], path[0][1], path[1][0], path[1][1], s[0], s[1]):
                         path[0] = (s[0], s[1])
                     else:
                         path.insert(0, (s[0], s[1]))
@@ -67,7 +52,7 @@ class Optimizer:
                     del segs[idx]
                     idx = -1
                 elif fequals(path[-1][0], s[0]) and fequals(path[-1][1], s[1]):
-                    if self.colinear(path[-2], path[-1], (s[2], s[3])):
+                    if colinear2d(path[-2][0], path[-2][1], path[-1][0], path[-1][1], s[2], s[3]):
                         path[-1] = (s[2], s[3])
                     else:
                         path.append((s[2], s[3]))
@@ -75,7 +60,7 @@ class Optimizer:
                     del segs[idx]
                     idx = -1
                 elif fequals(path[-1][0], s[2]) and fequals(path[-1][1], s[3]):
-                    if self.colinear(path[-2], path[-1], (s[0], s[1])):
+                    if colinear2d(path[-2][0], path[-2][1], path[-1][0], path[-1][1], s[0], s[1]):
                         path[-1] = (s[0], s[1])
                     else:
                         path.append((s[0], s[1]))
--- util.py
+++ util.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import math
+
 def fequals(a, b, tolerance = 0.000001):
     """ Returns true if a (almost) equals b according to the given tolerance """
     return (a - tolerance <= b) and (a + tolerance >= b) 
@@ -8,3 +10,17 @@ def intercept2d(x0, y0, x1, y1, y, precision = 6):
     """ Apply the intercept theorem (Thales) in 2D """
     return round(x0 + (x1 - x0) * (y - y0) / (y1 - y0), precision) 
    
+def colinear2d(x0, y0, x1, y1, x2, y2):
+    """ Returns true if three 2d points are colinear """
+    
+    u, v = [ x1 - x0, y1 - y0 ], [ x2 - x0, y2 - y0 ]
+    
+    ulen = math.sqrt(u[0] * u[0] + u[1] * u[1])
+    vlen = math.sqrt(v[0] * v[0] + v[1] * v[1])
+    
+    if fequals(ulen, 0) or fequals(vlen, 0):
+        return True
+    
+    u1, v1 = [ u[0] / ulen, u[1] / ulen ], [ v[0] / vlen, v[1] / vlen ]
+    
+    return fequals(1, abs(u1[0] * v1[0] + u1[1] * v1[1]))
